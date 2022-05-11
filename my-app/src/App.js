f929599a7394665b9021a5293259162d340204e9
vinaymarni <86548675+vinaymarni@users.noreply.github.com>
2022-05-11 11:45:16 +0530

    Update App.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,12 @@
-import React, { useState } from "react";
+import React, { useState, Fragment } from "react";
 import './App.css';
 import Loader from 'react-loader-spinner'
 import 'react-loader-spinner/dist/loader/css/react-spinner-loader.css'
 
 function App() {
-  let customerDetails = 
-  {
-    id: "",
+  let customerDetails = {
+    id: 0,
+    customerId: "",
     name: "",
     country: "",
     logo_url: "",
@@ -14,9 +14,15 @@ function App() {
   const [url, setImage_url] = useState('')
   const [loading, setLoading] = useState(false)
   const [details, setDetails] = useState([])
+  const [idErrMsg, setIdErrMsg] = useState("")
+  const [nameErrMsg, setNameErrMsg] = useState("")
+  const [countryErrMsg, setCountryErrMsg] = useState("")
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  }
   const getCustomerId = (e) =>{  
-    customerDetails.id = e.target.value  
+    customerDetails.customerId = e.target.value
   }
   const getCustomerName = (e) =>{
     customerDetails.name = e.target.value
@@ -24,7 +30,22 @@ function App() {
   const getCustomerCountry = (e) =>{
     customerDetails.country = e.target.value
   }
-  
+
+  const uploadDetails = () => {
+    customerDetails.id = details.length + 1
+    const { customerId, name, country, logo_url } = customerDetails;
+
+    if ((customerId !== "") && (name !== "") && (country !== "") && (logo_url !== "")){
+      
+      setDetails(oldDetails => [...oldDetails, customerDetails])
+      
+    }else{
+      console.log("Please fill all details")
+      alert("Please fill all details")
+    }
+  }
+  console.log(details, details.length)
+
   const uploadImage = async e => {
     const files = e.target.files
     const data = new FormData()
@@ -36,78 +57,108 @@ function App() {
     const config = {
       method: "POST",
       body: data 
-  };
+    };
     const res = await fetch(imgurl, config)
     const file = await res.json()
     setImage_url(file.url)
-    setLoading(false)
-    customerDetails.logo_url = file.url  
-    setDetails([...details, customerDetails]) 
+    customerDetails.logo_url = file.url
+    setLoading(false)    
+    uploadDetails()
   }
   const DeleteLogo = () =>{
     setImage_url(null)
   }
-  console.log(details)
+
   return (
-    <div className="main-container">
-      <div className="App">
-        <h1 className="main-heading">Portal Configuration</h1>
-        <p className='content'>
-          We're absolutely thrilled to have you in on board and can't wait to <br/> see you monitor your storage resources seamlessly
-        </p>       
-        <div className="inner-container">
-          <h2 className="organization-heading">Organization information</h2>
-          <div className="box-order">
-              <div className="profile-box" >
-                <div className="profile-container">
-                  <h2 className="logo-heading">Add logo</h2>
-                  <div className="profile" >
-                    {url ? (
-                      loading ? (
-                        <Loader type="TailSpin" color="#00BFFF" height={50} width={60} />
-                         ) : ( <img src={url} alt="" className="profile" />)
-                    ) : (
-                      <img alt="" src='https://res.cloudinary.com/di01osmzz/image/upload/v1649318682/bx_bxs-image-add_is04eg.png' />
-                    )}                                  
+    <Fragment>
+      <div className="main-container">
+        <div className="App">
+          <form onSubmit={handleSubmit}>
+          <h1 className="main-heading">Portal Configuration</h1>
+          <p className='content'>
+            We're absolutely thrilled to have you in on board and can't wait to <br/> see you monitor your storage resources seamlessly
+          </p>       
+          <div className="inner-container">
+            <h2 className="organization-heading">Organization information</h2>
+            <div className="box-order">
+                <div className="profile-box" >
+                  <div className="profile-container">
+                    <h2 className="logo-heading">Add logo</h2>
+                    <div className="profile" >
+                      {url ? (
+                        loading ? (
+                          <Loader type="TailSpin" color="#00BFFF" height={50} width={60} />
+                          ) : ( <img src={url} alt="" className="profile" />)
+                      ) : (
+                        <img alt="" src='https://res.cloudinary.com/di01osmzz/image/upload/v1649318682/bx_bxs-image-add_is04eg.png' />
+                      )}                                  
+                    </div>
+                    <h3 className='resolution'>Resolution 800x800 px</h3>
                   </div>
-                  <h3 className='resolution'>Resolution 800x800 px</h3>
-                </div>
-                <div className="button-container">                              
-                  <div>
-                    <button className="delete" type='button' onClick={DeleteLogo} >
-                      <img alt="" src="https://res.cloudinary.com/di01osmzz/image/upload/v1649399143/delete_pp824j.png" className="delete-icon" />
-                      Delete
-                    </button>                   
-                    <input
-                      id="logo"
-                      type="file"
-                      onChange={uploadImage}
-                      style={{display: 'none'}}
-                    />                   
-                    <button type='file' className="browse">
-                      <label htmlFor="logo" >
-                        <img alt="" src="https://res.cloudinary.com/di01osmzz/image/upload/v1649396968/Group_13135_xjem2h.png" className="icon" />
-                        Browse
-                      </label>
-                    </button>                    
+                  <div className="button-container">                              
+                    <div>
+                      <button className="delete" type='button' onClick={DeleteLogo} >
+                        <img alt="" src="https://res.cloudinary.com/di01osmzz/image/upload/v1649399143/delete_pp824j.png" className="delete-icon" />
+                        Delete
+                      </button>                   
+                      <input
+                        id="logo"
+                        type="file"
+                        onChange={uploadImage}
+                        style={{display: 'none'}}
+                      />                   
+                      <button type='file' className="browse">
+                        <label htmlFor="submit">
+                        <label htmlFor="logo" >
+                          <img alt="" src="https://res.cloudinary.com/di01osmzz/image/upload/v1649396968/Group_13135_xjem2h.png" className="icon" />
+                          Browse 
+                        </label>
+                        </label>
+                      </button>                    
+                    </div>
                   </div>
                 </div>
-              </div>
-              <div className="details-box" >
-                <h2 className="logo-heading">Add details</h2>
-                <input type="text" className='input' placeholder="Customer ID" onChange={getCustomerId} />
-                <input type="text" className='input' placeholder="Customer Name" onChange={getCustomerName} />              
-                <input list="countries-drop-down" type="text" className="input" placeholder="Country" onChange={getCustomerCountry} />
-                <datalist className="input"  id="countries-drop-down">
-                  <option value="America" >America</option>
-                  <option value="India">India</option>
-                  <option value="Australia">Australia</option>
-                </datalist>                
-              </div>
-          </div>
-        </div> 
+                <div className="details-box" >
+                  <h2 className="logo-heading">Add details</h2>
+                  <input 
+                    type="text" 
+                    value={details.id} 
+                    className='input' 
+                    placeholder="Customer ID" 
+                    onChange={getCustomerId} 
+                    onBlur={e => {(e.target.value === "") ? setIdErrMsg("*ID is required") : setIdErrMsg("") }}
+                    required />
+                  <p style={{color:"red"}}>{idErrMsg}</p>
+                  <input 
+                    type="text" 
+                    value={details.name} 
+                    className='input' 
+                    placeholder="Customer Name" 
+                    onChange={getCustomerName} 
+                    onBlur={e => {(e.target.value === "")? setNameErrMsg("*Name is required") : setNameErrMsg("") }}
+                    required />   
+                  <p style={{color:"red"}}>{nameErrMsg}</p>           
+                  <input 
+                    list="countries-drop-down" 
+                    type="text" className="input" 
+                    placeholder="Country" 
+                    onChange={getCustomerCountry} 
+                    onBlur={e => {(e.target.value === "")? setCountryErrMsg("*Country is required") : setCountryErrMsg("") }}
+                    required />
+                  <p style={{color:"red"}}>{countryErrMsg}</p>
+                  <datalist className="input"  id="countries-drop-down">
+                    <option value="America" >America</option>
+                    <option value="India">India</option>
+                    <option value="Australia">Australia</option>
+                  </datalist>   
+                  <input style={{display: 'none'}} id="submit" type="submit"></input>             
+                </div>
+            </div>
+          </div> 
+          </form>
+        </div>
       </div>
-    </div>
+    </Fragment>
   );
 }
 
